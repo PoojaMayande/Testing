d10c62625cd0d59c7f2528f3416615108e4f364b
Gajanan29 <115784416+Gajanan29@users.noreply.github.com>
2022-10-15 13:17:06 +0530

    Update GitDemo.java
    By smita

--- Git/src/GitPakage/GitDemo.java
+++ Git/src/GitPakage/GitDemo.java
@@ -1,7 +1,14 @@
 package GitPakage;
 
 public class GitDemo {
-public static void main(String[] args) {
+public static void main(String[] args)
+{
 	System.out.println("Main Git");
 }
+	public void m1()
+	{
+	
+	System.out.println("Main Git");
+	
+	}
 }
